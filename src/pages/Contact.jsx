a570ebaa068e6d87f15308fadbb1a02015fb03d1
Tuan Nguyen <nvtuan39202@gmail.com>
2023-12-17 23:09:44 +0700

    done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,6 +4,11 @@ import { useSelector } from 'react-redux'
 
 const Contact = () => {
     const user = useSelector(state => state.auth.user)
+    console.log(user);
+
+    // Check if user and user.name are defined
+    const userName = user && user.name ? user.name.firstname : 'Your Name';
+
     return (
         <div>
             <div className="container mb-5">
@@ -21,7 +26,7 @@ const Contact = () => {
                         <form>
                             <div className="mb-3">
                                 <label htmlFor="exampleForm" className="form-label">Full Name</label>
-                                <input type="text" className="form-control" id="exampleForm" placeholder={user.name.firstname + ' ' + user.name.lastname} />
+                                <input type="text" className="form-control" id="exampleForm" placeholder={userName} />
                             </div>
                             <div className="mb-3">
                                 <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
